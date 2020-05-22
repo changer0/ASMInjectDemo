401cb14f155a3a5a51ffaea4ee4e140a5e486e80
p_zlulzhang <zhanglulu@yuewen.com>
2020-05-22 12:05:15 +0800

    独立 module 插件 CustomPluginB

---
+++ asmplugin/src/main/groovy/com/lulu/customplugin/CustomExtensionB.groovy
@@ -0,0 +1,5 @@
+package com.lulu.customplugin
+
+class CustomExtensionB {
+    String extensionArgs = ""
+}
---
+++ asmplugin/src/main/groovy/com/lulu/customplugin/CustomPluginB.groovy
@@ -0,0 +1,22 @@
+package com.lulu.customplugin
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * 自定义插件
+ */
+class CustomPluginB implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        println "CustomPluginB Hello"
+        //将Extension注册给Plugin
+        def extension = project.extensions.create("customB", CustomExtensionB)
+        //定义一个任务
+        project.task('CustomPluginTaskB') {
+            doLast {
+                println "接收外部参数：${extension.extensionArgs}"
+            }
+        }
+    }
+}
